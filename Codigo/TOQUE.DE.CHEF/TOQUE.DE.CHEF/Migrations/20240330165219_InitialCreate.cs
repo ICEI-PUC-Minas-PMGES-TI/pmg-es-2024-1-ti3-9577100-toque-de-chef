f6ca773624d73875f0a914273d9396478da8c533
Lara <laralima.dev@gmail.com>
2024-03-30 15:34:27 -0300

    Adiciona migration

---
+++ Codigo/TOQUE.DE.CHEF/TOQUE.DE.CHEF/Migrations/20240330165219_InitialCreate.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TOQUE.DE.CHEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CATEGORIES",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NAME = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DESCRIPTION = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CATEGORIES", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SUPPLIERS",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    NAME = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EMAIL = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PHONE = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DESCRIPTION = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SUPPLIERS", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CATEGORIES");
+
+            migrationBuilder.DropTable(
+                name: "SUPPLIERS");
+        }
+    }
+}
