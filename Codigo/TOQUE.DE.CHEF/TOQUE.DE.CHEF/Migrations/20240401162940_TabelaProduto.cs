073e2321f07dc08e24c17ac5ea25762068b3377f
Lara <laralima.dev@gmail.com>
2024-04-01 13:34:12 -0300

    volta com as configuraçoes do program

---
+++ Codigo/TOQUE.DE.CHEF/TOQUE.DE.CHEF/Migrations/20240401162940_TabelaProduto.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TOQUE.DE.CHEF.Migrations
+{
+    /// <inheritdoc />
+    public partial class TabelaProduto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PASSWORD",
+                table: "Users",
+                type: "nvarchar(100)",
+                maxLength: 100,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(50)",
+                oldMaxLength: 50);
+
+            migrationBuilder.CreateTable(
+                name: "PRODUCTS",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CATEGORY_ID = table.Column<int>(type: "int", nullable: false),
+                    NAME = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DESCRIPTION = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    UNIT_PRICE = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PRODUCTS", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PRODUCTS");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PASSWORD",
+                table: "Users",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(100)",
+                oldMaxLength: 100);
+        }
+    }
+}
