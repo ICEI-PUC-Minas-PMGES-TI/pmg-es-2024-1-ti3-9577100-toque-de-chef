5ff49a364607b1258d3586a2c34fbad2d9c5a849
Pedro Oliveira <pedrophmo1205@gmail.com>
2024-04-01 09:39:30 -0300

    #FEATS: Começa CRUD de fornecedores

---
+++ Codigo/TOQUE.DE.CHEF/TOQUE.DE.CHEF/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TOQUE.DE.CHEF.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Codigo/TOQUE.DE.CHEF/TOQUE.DE.CHEF/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TOQUE.DE.CHEF.Models
+{
+    [Table("PRODUCTS")]
+    public class Product
+    {
+        //atributos
+        [Column("ID")]
+        public int Id { get; set; }
+        [Column("CATEGORY_ID")]
+        public int Category_id { get; set; }
+        [Column("NAME")]
+        public string Name { get; set; }
+        [Column("DESCRIPTION")]
+        public string Description { get; set; }
+        [Column("UNIT_PRICE")]
+        public double Unit_Price { get; set; }
+    }
+}
